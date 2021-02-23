32e5d9142e9898f2f8fa3b08a224358696987e5a
JohnnyJohnJohns <johnhealy123@gmail.com>
2021-02-23 11:51:07 +0000

    Added Player movement and platforms.

--- 2D Testing/Assets/Scripts/PlayerMovement.cs
+++ 2D Testing/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,54 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveForce, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+    private bool canJump;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveAxis = Vector2.right * moveDir;
+
+        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
+        {
+            myRB.AddForce(moveAxis * moveForce, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
+        moveDir = context.ReadValue<float>();
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
+        if (canJump)
+        {
+            myRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
     }
     
 }
